eb2c8906bb8de7f67b2a9b462067cd1a436175d0
Andrej Boso <a.boso@donboscosandona.it>
2025-06-18 15:38:28 +0200

    iniziosei

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "/home", to: "home#index", as: "home"
-  get "spagnia", to: "home#spagnia", as: "spagnia"
-  get "francia", to: "home#francia", as: "francia"
-  get "about", to: "home#about", as: "about"
-  get "contact", to: "home#contact", as: "contact"
+  get "/spagnia", to: "home#spagnia", as: "spagnia"
+  get "/francia", to: "home#francia", as: "francia"
+  get "/about", to: "home#about", as: "about"
+  get "/contact", to: "home#contact", as: "contact"
+  root "home#index"
 end
